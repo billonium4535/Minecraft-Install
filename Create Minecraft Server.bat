a0ff49197ae0051a383ee45bfec086e9cc8204b8
billonium4535 <billy.sethgrimmett@gmail.com>
2023-01-10 17:34:51 +0000

    Commented Vanilla and updated README.md

--- Create Minecraft Server.bat
+++ Create Minecraft Server.bat
@@ -1,12 +1,26 @@
 @echo off
+REM Stops the user being able to see commands
+
+set ServerFile="https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar"
+REM Sets the server file to download
+
 mkdir "Minecraft Server"
 cd "Minecraft Server"
+REM Creates a directory and enters it
+
 set cwd=%cd%
-curl https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar --output "%cwd%\server.jar"
+REM Sets the new directory to the working directory
+
+curl %ServerFile% --output "%cwd%\server.jar"
 start server.jar
+REM Downloads the server file and starts it
+
 :FindEULA
 if exist eula.txt goto SetTrue
 goto FindEULA
+REM Function to check if the EULA has been generated
+
 :SetTrue
 echo eula=true> eula.txt
 java -Xmx8G -Xms2G -jar server.jar nogui
+REM Sets the EULA to true and re-launches the server
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Minecraft-Install
 Batch files for automatically creating minecraft servers.
+
+## Create Minecraft Server.bat
+This file is to create a vanilla minecraft server.\
+To create the latest version server, copy the link from [minecraft's website](https://www.minecraft.net/en-us/download/server) that says minecraft_server.x.x.x.jar.
+<div>
+The link will look like "https://piston-data.mojang.com/v1/objects/.../server.jar"<br>
+Paste the link into the ServerFile variable.
+</div>
+
+## Create Paper Minecraft Server.bat
+This file...
